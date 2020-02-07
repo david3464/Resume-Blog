f82223a94242652609437b538ec808997c9ee233
David <davidchen3501@gmail.com>
2020-02-07 12:01:03 -0500

    20200207 Initial Set Up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Resume-Blog
-resume home page
+# 1.1-KC_Resume_Blog
+Build Online Resume Page
+2020 01 17 Initial Login Page and Home Page
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+//app.use('/', indexRouter);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('Index/Login', { layout: 'page_layout/Login_layout'});
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('Index/home',{ layout: 'page_layout/Resume_layout'});
+});
+
+router.get('/work_exp', function(req, res, next) {
+  res.render('Index/work_exp',{ layout: 'page_layout/Resume_layout'});
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('Index/blog',{ layout: 'page_layout/Resume_layout'});
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('Index/gallery',{ layout: 'page_layout/Resume_layout'});
+});
+
+router.get('/testimonial', function(req, res, next) {
+  res.render('Index/testimonial',{ layout: 'page_layout/Resume_layout'});
+});
+
+
+router.get('/contact', function(req, res, next) {
+  res.render('Index/contact',{ layout: 'page_layout/Resume_layout'});
+});
+
+module.exports = router;
